bacb01e3131f344c06d9926c60298e96f264c0b4
Sílvio Silva <gasparsilvio7@gmail.com>
2025-02-27 22:56:05 -0500

    Refactor: code style improved, some fixes and more support to SQLite added

--- src/Boot/Configs.php
+++ src/Boot/Configs.php
@@ -1,55 +1,62 @@
 <?php
 
+require_once __DIR__ . "/../../vendor/autoload.php";
+
 use Dotenv\Dotenv;
+use Silviooosilva\CacheerPhp\Core\Connect;
 use Silviooosilva\CacheerPhp\Helpers\EnvHelper;
-
-    $rootPath = EnvHelper::getRootPath();
-    $dotenv = Dotenv::createImmutable($rootPath);
-    $dotenv->load();
-
-    $Connection = $_ENV['DB_CONNECTION'] ?? 'mysql';
-    $Host = $_ENV['DB_HOST'] ?? 'localhost';
-    $Port = $_ENV['DB_PORT'] ?? '3306';
-    $DBName = $_ENV['DB_DATABASE'] ?? 'cacheer_db';
-    $User = $_ENV['DB_USERNAME'] ?? 'root';
-    $Password = $_ENV['DB_PASSWORD'] ?? '';
+use Silviooosilva\CacheerPhp\Helpers\SqliteHelper;
 
 
-    define('CACHEER_DATABASE_CONFIG', [
-        "mysql" => [
-            "driver" => $Connection,
-            "host" => $Host,
-            "port" => $Port,
-            "dbname" => $DBName,
-            "username" => $User,
-            "passwd" => $Password,
-            "options" => [
-                PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8",
-                PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
-                PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_OBJ,
-                PDO::ATTR_CASE => PDO::CASE_NATURAL
-            ]
-        ],
-        "sqlite" => [
-            "driver" => "sqlite",
-            "dbname" => "/caminho/para/o/banco.sqlite",
-            "options" => [
-                PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
-                PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_OBJ,
-                PDO::ATTR_CASE => PDO::CASE_NATURAL
-            ]
-        ],
-        "pgsql" => [
-            "driver" => $Connection,
-            "host" => $Host,
-            "port" => $Port,
-            "dbname" => $DBName,
-            "username" => $User,
-            "passwd" => $Password,
-            "options" => [
-                PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
-                PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_OBJ,
-                PDO::ATTR_CASE => PDO::CASE_NATURAL
-            ]
-        ],
-    ]);
+$rootPath = EnvHelper::getRootPath();
+$dotenv = Dotenv::createImmutable($rootPath);
+$dotenv->load();
+
+
+$Connection = $_ENV['DB_CONNECTION'] ?? 'mysql';
+$Host       = $_ENV['DB_HOST'] ?? 'localhost';
+$Port       = $_ENV['DB_PORT'] ?? '3306';
+$DBName     = $_ENV['DB_DATABASE'] ?? 'cacheer_db';
+$User       = $_ENV['DB_USERNAME'] ?? 'root';
+$Password   = $_ENV['DB_PASSWORD'] ?? '';
+
+Connect::setConnection($Connection);
+
+define('CACHEER_DATABASE_CONFIG', [
+    "mysql" => [
+        "driver"  => $Connection,
+        "host"    => $Host,
+        "port"    => $Port,
+        "dbname"  => $DBName,
+        "username"=> $User,
+        "passwd"  => $Password,
+        "options" => [
+            PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8",
+            PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
+            PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_OBJ,
+            PDO::ATTR_CASE               => PDO::CASE_NATURAL
+        ]
+    ],
+    "sqlite" => [
+        "driver"  => $Connection,
+        "dbname"  => SqliteHelper::database($DBName),
+        "options" => [
+            PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
+            PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_OBJ,
+            PDO::ATTR_CASE               => PDO::CASE_NATURAL
+        ]
+    ],
+    "pgsql" => [
+        "driver"  => $Connection,
+        "host"    => $Host,
+        "port"    => $Port,
+        "dbname"  => $DBName,
+        "username"=> $User,
+        "passwd"  => $Password,
+        "options" => [
+            PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
+            PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_OBJ,
+            PDO::ATTR_CASE               => PDO::CASE_NATURAL
+        ]
+    ],
+]);
