b892652ca3d546c0c096093cbccbd94192f5c51e
Sílvio Silva <gasparsilvio7@gmail.com>
2025-03-07 22:31:57 -0500

    Refactor: Improved logs creation

--- src/Utils/CacheDriver.php
+++ src/Utils/CacheDriver.php
@@ -4,40 +4,62 @@ namespace Silviooosilva\CacheerPhp\Utils;
 
 use Exception;
 use Silviooosilva\CacheerPhp\Cacheer;
-use Silviooosilva\CacheerPhp\CacheStore\DatabaseCacheStore;
 use Silviooosilva\CacheerPhp\CacheStore\FileCacheStore;
+use Silviooosilva\CacheerPhp\CacheStore\RedisCacheStore;
+use Silviooosilva\CacheerPhp\CacheStore\DatabaseCacheStore;
+use Silviooosilva\CacheerPhp\Exceptions\CacheFileException;
+use Silviooosilva\CacheerPhp\Helpers\EnvHelper;
 
-
+/**
+ * Class CacheDriver
+ * @author Sílvio Silva <https://github.com/silviooosilva>
+ * @package Silviooosilva\CacheerPhp
+ */
 class CacheDriver
 {
 
     /**
-     * @var Cacheer 
+     * @var Cacheer
      */
     protected $cacheer;
+
+    /** @param string $logPath */
+    public string $logPath = 'cacheer.log';
 
     public function __construct(Cacheer $cacheer)
     {
         $this->cacheer = $cacheer;
     }
 
-    public function useDatabaseDriver(string $logPath = 'cacheer.log')
+    /**
+     * @return Cacheer
+     */
+    public function useDatabaseDriver()
     {
-        $this->cacheer->cacheStore = new DatabaseCacheStore($logPath);
-        return $this;
+        $this->cacheer->cacheStore = new DatabaseCacheStore($this->logPath);
+        return $this->cacheer;
     }
 
     /**
      * @throws \Exception
      * @return Cacheer
      */
-    public function useFileDriver(string $logPath = 'cacheer.log')
+    public function useFileDriver()
     {
         if (!isset($this->cacheer->options['cacheDir'])) {
-            throw new Exception("The 'cacheDir' option is required for the file driver.");
+            throw CacheFileException::create("The 'cacheDir' option is required for the file driver.");
         }
-        $this->cacheer->options['loggerPath'] = $logPath;
+        $this->cacheer->options['loggerPath'] = $this->logPath;
         $this->cacheer->cacheStore = new FileCacheStore($this->cacheer->options);
+        return $this->cacheer;
+    }
+
+    /**
+     * @return Cacheer
+     */
+    public function useRedisDriver()
+    {
+        $this->cacheer->cacheStore = new RedisCacheStore($this->logPath);
         return $this->cacheer;
     }
 
@@ -47,9 +69,27 @@ class CacheDriver
     public function useDefaultDriver()
     {
         if (!isset($this->cacheer->options['cacheDir'])) {
-            $this->cacheer->options['cacheDir'] = __DIR__ . "/../../Examples/cache";
+            $cacheDir = EnvHelper::getRootPath() . "CacheerPHP/Cache";
+            if ($this->isDir($cacheDir)) {
+                $this->cacheer->options['cacheDir'] = $cacheDir;
+
+            } else {
+                throw CacheFileException::create("Failed to create cache directory: " . $cacheDir);
+            }
         }
         $this->useFileDriver();
         return $this->cacheer;
+    }
+
+    /**
+    * @param mixed $dirName
+    * @return bool
+    */
+    private function isDir(mixed $dirName)
+    {
+      if (is_dir($dirName)) {
+          return true;
+      }
+      return mkdir($dirName, 0755, true);
     }
 }
