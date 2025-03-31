80735881cce4c41d81f85f0ab530eec8494df2d3
Sílvio Silva <gasparsilvio7@gmail.com>
2025-03-31 09:26:57 -0400

    TimeBuilder Introduction

---
+++ src/Support/TimeBuilder.php
@@ -0,0 +1,96 @@
+<?php
+
+namespace Silviooosilva\CacheerPhp\Support;
+
+use Closure;
+use Silviooosilva\CacheerPhp\CacheStore\CacheManager\OptionBuilders\FileOptionBuilder;
+
+/**
+ * Class TimeBuilder
+ * @author Sílvio Silva <https://github.com/silviooosilva>
+ * @package Silviooosilva\CacheerPhp
+ */
+class TimeBuilder
+{
+
+  /** @param string $unit */
+  private string $unit;
+
+  /** @param int $value */
+  private int $value;
+
+  /** @param Closure $callback */
+  private Closure $callback;
+
+  /** @param FileOptionBuilder */
+  private $builder = null;
+
+  public function __construct(Closure $callback, $builder)
+  {
+    $this->callback = $callback;
+    $this->builder = $builder;
+  }
+
+  /**
+  * @param int $value
+  * @return mixed
+  */
+  public function second(int $value) {
+    return $this->setTime($value, "seconds");
+  }
+
+  /**
+  * @param int $value
+  * @return mixed
+  */
+  public function minute(int $value) {
+    return $this->setTime($value, "minutes");
+  }
+
+  /**
+  * @param int $value
+  * @return mixed
+  */
+  public function hour(int $value) {
+    return $this->setTime($value, "hours");
+  }
+
+  /**
+  * @param int $value
+  * @return mixed
+  */
+  public function day(int $value) {
+    return $this->setTime($value, "days");
+  }
+
+  /**
+  * @param int $value
+  * @return mixed
+  */
+  public function week(int $value) {
+    return $this->setTime($value, "weeks");
+  }
+
+  /**
+  * @param int $value
+  * @return mixed
+  */
+  public function month(int $value) {
+    return $this->setTime($value, "months");
+  }
+  
+
+  /**
+  * @param int $value
+  * @param string $unit
+  * @return void
+  */
+  private function setTime(int $value, string $unit) {
+
+    $this->value = $value;
+    $this->unit = $unit;
+   ($this->callback)("{$value} {$unit}");
+    return $this->builder;
+  }
+
+}
