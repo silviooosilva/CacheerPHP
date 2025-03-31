77f7f06112c09c43ef96538e79b9a3bfdcc7c8b2
Sílvio Silva <gasparsilvio7@gmail.com>
2025-03-31 09:24:44 -0400

    Style[OptionBuilder]: Code style improved

--- src/Config/Option/Builder/OptionBuilder.php
+++ src/Config/Option/Builder/OptionBuilder.php
@@ -4,8 +4,11 @@ namespace Silviooosilva\CacheerPhp\Config\Option\Builder;
 
 use Silviooosilva\CacheerPhp\CacheStore\CacheManager\OptionBuilders\FileOptionBuilder;
 
-
-
+/**
+ * Class OptionBuilder
+ * @author Sílvio Silva <https://github.com/silviooosilva>
+ * @package Silviooosilva\CacheerPhp
+ */
 class OptionBuilder
 {
   /**
