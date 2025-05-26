dba87bd26ba3e0596551f3b592857a0402e6502e
silviooosilva <gasparsilvio7@gmail.com>
2025-05-26 00:09:56 -0400

    refactor/Cacheer-Exceptions-Improved

--- src/Exceptions/CacheDatabaseException.php
+++ src/Exceptions/CacheDatabaseException.php
@@ -11,8 +11,12 @@ class CacheDatabaseException extends BaseException
   private static string $before = "<Database Cache Store Exception>";
 
   /**
-  * @return void
-  */
+   * @param string $message
+   * @param int $code
+   * @param Exception|null $previous
+   * @param array $details
+   * @return self
+   */
   public static function create(string $message = "", int $code = 0, ?Exception $previous = null, array $details = [])
   {
     return new self(self::getBefore() . ": " .$message, $code, $previous, $details);
@@ -22,7 +26,7 @@ class CacheDatabaseException extends BaseException
   /**
   * @return string
   */
-  public static function getBefore()
+  public static function getBefore(): string
   {
     return self::$before;
   }
@@ -30,9 +34,33 @@ class CacheDatabaseException extends BaseException
   /**
   * @return void
   */
-  public static function setBefore(string $text)
+  public static function setBefore(string $text): void
   {
     self::$before = $text;
   }
 
+   /*
+    * @return array
+    */
+    public function toArray()
+    {
+        return parent::toArray();
+    }
+    
+    /**
+     * @return string
+     */
+    public function jsonSerialize(): array
+    {
+        return parent::jsonSerialize();
+    }
+
+    /**
+     * @param int $options
+     * @return string
+     */
+    public function toJson(int $options = 0)
+    {
+      return parent::toJson($options);
+    }
 }
--- src/Exceptions/ConnectionException.php
+++ src/Exceptions/ConnectionException.php
@@ -6,32 +6,58 @@ use Exception;
 
 class ConnectionException extends BaseException
 {
+    private static string $before = "<Connection Exception>";
 
-  /** @param string $before */
-  private static string $before = "<Connection Exception>";
+    /**
+     * @param string $message
+     * @param int $code
+     * @param Exception|null $previous
+     * @param array $details
+     * @return self
+     */
+    public static function create(string $message = "", int $code = 0, ?Exception $previous = null, array $details = [])
+    {
+        return new self(self::getBefore() . ": " . $message, $code, $previous, $details);
+    }
 
-  /**
-  * @return void
-  */
-  public static function create(string $message = "", int $code = 0, ?Exception $previous = null, array $details = [])
-  {
-    return new self(self::getBefore() . ": " .$message, $code, $previous, $details);
-  }
+    /**
+     * @return string
+     */
+    public static function getBefore(): string
+    {
+        return self::$before;
+    }
 
-  /**
-  * @return string
-  */
-  public static function getBefore()
-  {
-    return self::$before;
-  }
+    /**
+     * @param string $text
+     */
+    public static function setBefore(string $text): void
+    {
+        self::$before = $text;
+    }
 
-  /**
-  * @return void
-  */
-  public static function setBefore(string $text)
-  {
-    self::$before = $text;
-  }
+    /*
+    * @return array
+    */
+    public function toArray()
+    {
+        return parent::toArray();
+    }
+    
+    /**
+     * @return string
+     */
+    public function jsonSerialize(): array
+    {
+        return parent::jsonSerialize();
+    }
 
+    /**
+     * @param int $options
+     * @return string
+     */
+    public function toJson(int $options = 0)
+    {
+      return parent::toJson($options);
+    }
 }
